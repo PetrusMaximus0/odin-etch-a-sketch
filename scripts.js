8aa8d0d4de4c908761e4cc1dbf6b2c5f5b6ff98b
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2023-09-15 19:51:22 +0200

    add drawning and hover functionality

--- scripts.js
+++ scripts.js
@@ -1,22 +1,75 @@
-let cellNumber = 32;
-let cellSize = 0;//px
+//GLOBALS
 const GridSize = 512;
-//BEGIN generate grid FUNCTION
-//container.appendChild(grid);
-//
-//generate grid
 
-if(cellNumber>128) cellNumber = 128;
-cellSize = GridSize / cellNumber;
+//generates a grid with the input cell Numbers per side
+function generateGrid(cellNumber) {
+    if(cellNumber>128) cellNumber = 128;
+    cellSize = GridSize / cellNumber;
 
-const gridContainer = document.getElementById("grid-container");
-gridContainer.style.gridTemplateColumns = `repeat(${cellNumber}, ${cellSize}px)`;
-gridContainer.style.gridAutoRows = `${cellSize}px`;
+    const gridContainer = document.getElementById("grid-container");
+    gridContainer.style.gridTemplateColumns = `repeat(${cellNumber}, ${cellSize}px)`;
+    gridContainer.style.gridAutoRows = `${cellSize}px`;
 
-for (let i = 0; i<cellNumber*cellNumber; i++){
-    const cell = document.createElement('div');  
-    cell.classList.add('cell');
-    gridContainer.appendChild(cell);
+    for (let i = 0; i<cellNumber*cellNumber; i++){
+        const cell = document.createElement('div');      
+        gridContainer.appendChild(cell);
+    }
 }
 
+//toggles grid lines on or off on the sketch pad
+function toggleGridLines(){
+    const elements = document.querySelectorAll("#grid-container div");
+    elements.forEach(el => {
+        console.log(el.style.borderWidth);
+        if (el.style.borderWidth === "0px") 
+            el.style.borderWidth = "1px";
+        else 
+            el.style.borderWidth = "0px";
+    });
+}
+
+
+function changeCellColor(cell, newColor = "white"){
+    cell.target.style.backgroundColor = newColor;
+    tempColor = newColor;
+    console.log("painted!");
+}
+
+
+//event to switch colors on a square
+const gridContainer = document.getElementById("grid-container");
+let tempColor = "white";
+let brushColor = "black";
+let paintTimer = null;
+let mouseClicked = false;
+
+gridContainer.addEventListener('mouseover', function(e){
+    tempColor = e.target.style.backgroundColor;
+    e.target.style.backgroundColor = brushColor;
+    if(mouseClicked) tempColor = brushColor; 
+});
+
+gridContainer.addEventListener('mouseout', function(e){
+    e.target.style.backgroundColor = tempColor;
+});
+
+gridContainer.addEventListener('mousedown', function(){ 
+    mouseClicked =true;
+    console.log(mouseClicked);
+});
+
+gridContainer.addEventListener('mouseup', function(){ 
+    mouseClicked = false;
+    console.log(mouseClicked);
+});
+
+gridContainer.addEventListener('mouseleave', function(){
+    mouseClicked = false;
+    console.log("mouse left screen");
+});
+
+
+//Run the code
+generateGrid(16);
+//toggleGridLines();
 
